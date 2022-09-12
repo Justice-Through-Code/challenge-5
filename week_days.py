0eac48a5dfe5576e412156d83da957025351b4b5
Lillie Schachter <lillieschachter@gmail.com>
2022-09-12 15:29:52 -0500

    clarify instructions

--- week_days.py
+++ week_days.py
@@ -10,18 +10,19 @@ def day_of_week():
 
 
 def favorite_activities():
-    # 2.1 TODO: Now, instead of printing out the day, let's ask the user what their favorite thing
-    # to do is on that day of the week.
-    # Make sure to use an f-string so that the user knows which day they're being asked about.
+    # 2.1 TODO: Now, in a for loop, instead of just printing out the day,
+    # let's ask the user what their favorite thing to do is on that day of the week.
+    # NOTE: Make sure to use an f-string so that the user knows which day they're being asked about.
 
     # We should keep track of the user's favorite things to do so that we can print them out all together.
     # 2.2 TODO: ABOVE your for loop, create a new empty list to hold the user's favorite activities.
 
     # 2.3 TODO: Now, back in your for loop, append each of the user's answers into your new list.
-    # Print out the list after your loop to check if it got populated correctly.
+    # AFTER your loop, print out the list to check if it got populated correctly.
 
-    # 2.4: After your first loop, let's create a new one. As an example, let's say the user's favorite thing to
-    # do on Mondays is plan their week. This time, we want the output to be something like this:
+    # 2.4: After the code you've written so far, let's create a new for loop.
+    # As an example, let's say the user's favorite thing to do on Mondays is plan their week.
+    #  This time, we want the output to be something like this:
     # f'On Mondays, your favorite activity is to plan your week.'
     # We need information from both lists! Let's use the `range` function to loop through the indexes
     # of the items in the lists (this will work because the lists are the same length).
@@ -47,9 +48,9 @@ def favorite_activities():
 
 def temp_by_day():
     # 3 TODO: Write a program that loops through the days in the week. Each day, ask the user what the temperature
-    # is. If the temperature is below 50, tell the user to put on a jacket. Or, if the temperature is
-    # between 50 and 65, tell the user to put on a sweater. Finally, if the temperature is above 65,
-    # tell the user to put on some sunscreen.
+    # is. If the temperature is below 50, tell the user to 'Brr, put on a jacket!'. Or, if the temperature is
+    # between 50 and 65, tell the user to 'Cozy, grab a sweater'. Finally, if the temperature is above 65,
+    # tell the user to 'Put on some sunscreen!'.
 
 
 def temp_by_day_continuous():
